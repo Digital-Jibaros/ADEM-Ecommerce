28b906b2f00c40c86d58afd66989c7b19417dec6
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-22 17:56:31 -0300

    replace span with SVG
    This way I can add the necessary decoration (3 images) to the text.

--- modules/home.js
+++ modules/home.js
@@ -1,3 +1,5 @@
+const svgNs = 'http://www.w3.org/2000/svg';
+
 export default function home() {
   const home = [hero(), about(), categories()];
 
@@ -7,24 +9,45 @@ export default function home() {
 function hero() {
   const hero = document.createElement('section');
   const div = document.createElement('div');
+  const svg = document.createElementNS(svgNs, 'svg');
+  const svgText = document.createElementNS(svgNs, 'text');
+  const svgImg1 = document.createElementNS(svgNs, 'image');
+  const svgImg2 = document.createElementNS(svgNs, 'image');
+  const svgImg3 = document.createElementNS(svgNs, 'image');
   const para = document.createElement('p');
-  const span1 = document.createElement('span');
-  const span2 = document.createElement('span');
   const btn = document.createElement('button');
   const btnPara = document.createElement('p');
   const btnArrow = document.createElement('img');
 
-  span1.textContent = 'Grow';
-  span2.textContent = 'with us';
+  svgText.textContent = 'Grow';
+  svgText.setAttribute('x', 15);
+  svgText.setAttribute('y', 80);
+  svgImg1.setAttribute('x', 5);
+  svgImg1.setAttribute('y', 5);
+  svgImg1.setAttribute('width', 25*1.25);
+  svgImg1.setAttribute('height', 25*1.25);
+  svgImg1.setAttribute('href', '../media/images/plant-text-1.png');
+  svgImg2.setAttribute('x', 15);
+  svgImg2.setAttribute('y', 65);
+  svgImg2.setAttribute('width', 14*1.25);
+  svgImg2.setAttribute('height', 14*1.25);
+  svgImg2.setAttribute('href', '../media/images/plant-text-2.png');
+  svgImg3.setAttribute('x', 225);
+  svgImg3.setAttribute('y', 25);
+  svgImg3.setAttribute('width', 56.74*1.25);
+  svgImg3.setAttribute('height', 56.74*1.25);
+  svgImg3.setAttribute('href', '../media/images/plant-text-3.png');
+
+  para.textContent = 'with us';
   btnPara.textContent = 'Start Your Green Journey';
-  btnArrow.setAttribute('src', '../media/images/btn-arrow.png')
+  btnArrow.setAttribute('src', '../media/images/btn-arrow.png');
 
   hero.classList.add('home-hero');
   div.classList.add('home-hero-div');
 
-  para.append(span1, span2);
+  svg.append(svgImg1, svgImg2, svgImg3, svgText);
   btn.append(btnPara, btnArrow);
-  div.append(para, btn);
+  div.append(svg, para, btn);
   hero.append(div);
 
   return hero;
