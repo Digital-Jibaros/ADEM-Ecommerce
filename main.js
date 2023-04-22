11212c14778102e19d2a0c89613079ac58c5df6d
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-22 17:22:40 -0300

    import and append home module

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import header from "./modules/header.js";
+import home from "./modules/home.js";
 
 const body = document.querySelector('body');
+const main = document.createElement('main');
+
+main.append(
+  ...home(),
+);
 
 body.append(
   header(),
+  main,
 );
