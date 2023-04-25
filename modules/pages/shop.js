1348d93ed6a32e76546e2e3fa774f4b3d9030aeb
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-25 19:00:29 -0300

    add categories and banner elements

--- modules/pages/shop.js
+++ modules/pages/shop.js
@@ -6,6 +6,19 @@ export default function home() {
 
 function categories() {
   const section = document.createElement('section');
+  const categoriesList = document.createElement('ul');
+  const categories = ['All Collections', 'Cacti', 'Plants', 'Succulents'];
+
+  categories.forEach((category, i) => {
+    const li = document.createElement('li');
+    li.textContent = category;
+
+    if (i === 0) li.classList.add('active');
+    
+    categoriesList.append(li);
+  });
+
+  section.append(categoriesList);
 
   section.classList.add('shop-categories');
   
@@ -14,8 +27,13 @@ function categories() {
 
 function banner() {
   const section = document.createElement('section');
+  const title = document.createElement('h1');
+
+  title.textContent = 'All Collections';
 
   section.classList.add('shop-banner');
+
+  section.append(title);
   
   return section;
 }
