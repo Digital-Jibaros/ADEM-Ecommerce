43945c0f07c147b5545f5a7d54b7380173392987
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-26 00:29:07 -0300

    create module to store articles data

---
+++ articles.js
@@ -0,0 +1,50 @@
+export const articles = [
+  {
+    name: 'Moonbeam Marigold',
+    img: './media/images/catalog/moonbeam-marigold.png',
+    category: 'Plants',
+    price: '10',
+  },
+  {
+    name: 'Ember Echeveria',
+    img: './media/images/catalog/ember-echeveria.png',
+    category: 'Plants',
+    price: '12',
+  },
+  {
+    name: 'Crimson Crassula',
+    img: './media/images/catalog/crimson-crassula.png',
+    category: 'Plants',
+    price: '8',
+  },
+  {
+    name: 'Golden Gasteria',
+    img: './media/images/catalog/golden-gasteria.png',
+    category: 'Plants',
+    price: '15',
+  },
+  {
+    name: 'Rainbow Rhipsalis',
+    img: './media/images/catalog/rainbow-rhipsalis.png',
+    category: 'Plants',
+    price: '10',
+  },
+  {
+    name: 'Jade Jewel Orchid',
+    img: './media/images/catalog/jade-jewel-orchid.png',
+    category: 'Succulents',
+    price: '25',
+  },
+  {
+    name: 'Midnight Moss',
+    img: './media/images/catalog/midnight-moss.png',
+    category: 'Succulents',
+    price: '5',
+  },
+  {
+    name: 'Aquamarine Aloe',
+    img: './media/images/catalog/aquamarine-aloe.png',
+    category: 'Succulents',
+    price: '15',
+  },
+];
